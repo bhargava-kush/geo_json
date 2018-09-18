f4aa8d6a546a93b87db4b5a23790fc4a2952dc7a
kush bhargava <bhargavakush93@gmail.com>
2018-09-18 11:19:30 +0530

    change library path in base setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -263,4 +263,5 @@ SOCIALACCOUNT_ADAPTER = 'geo_json.users.adapters.SocialAccountAdapter'
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-
+GDAL_LIBRARY_PATH = '/usr/local/lib/libgdal20'
+GEOS_LIBRARY_PATH = '/usr/local/lib/libgeos-3.5.0'
